312b9129aa04b72cf2b4f1346a163f444709db36
CoderJu <1483991357@qq.com>
2017-07-16 22:03:31 +0800

    继承结构映射使用一张表的方式，joined-subclass，一个类一张表

---
+++ src/cn/eleven/Inherit2/Reply.java
@@ -0,0 +1,19 @@
+package cn.eleven.Inherit2;
+
+import cn.eleven.Inherit2.Article;
+
+/**
+ * Created by User on 2017/7/13.
+ */
+public class Reply extends Article {
+
+    private int floor;
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+}
---
+++ src/cn/eleven/Inherit2/Topic.java
@@ -0,0 +1,18 @@
+package cn.eleven.Inherit2;
+
+import cn.eleven.Inherit2.Article;
+
+/**
+ * Created by User on 2017/7/13.
+ */
+public class Topic extends Article {
+    private  int type;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
